21b0c1fc29f9099862b8feb749c84ba4daa2ba8a
Eli Fuchsman <efuchsman@gmail.com>
2022-09-05 12:33:27 -0600

    Create count method for inventory and test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -13,7 +13,11 @@ RSpec.describe Dealership do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
 
     expect(dealership.inventory).to eq([])
-
   end
 
+  it 'can keep track of number of cars in inventory' do
+    dealership = Dealership.new("Acme Auto", "123 Main Street")
+
+    expect(dealership.inventory_count).to eq(0)
+  end
 end
