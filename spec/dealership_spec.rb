f405b66e403c692de9d9484e8456331e34bcb218
Eli Fuchsman <efuchsman@gmail.com>
2022-09-05 12:43:45 -0600

    Update has_inventory? test

--- spec/dealership_spec.rb
+++ spec/dealership_spec.rb
@@ -35,8 +35,19 @@ RSpec.describe Dealership do
 
   it 'returns true or false depending if there is any inventory' do
     dealership = Dealership.new("Acme Auto", "123 Main Street")
+    car_1 = Car.new("Ford Mustang", 1500, 36)
+    car_2 = Car.new("Toyota Prius", 1000, 48)
+    car_3 = Car.new("Toyota Tercel", 500, 48)
+    car_4 = Car.new("Chevrolet Bronco", 1250, 24)
 
     expect(dealership.has_inventory?).to be false
+
+    dealership.add_car(car_1)
+    dealership.add_car(car_2)
+    dealership.add_car(car_3)
+    dealership.add_car(car_4)
+
+    expect(dealership.has_inventory?).to be true
   end
 
 end
